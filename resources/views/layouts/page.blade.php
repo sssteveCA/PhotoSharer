b388911b6d039aa4a73692fbd8b11587a2390e2b
stepp <stepp@mail.com>
2023-03-10 21:49:10 +0100

    Added resources/views/partials/menu.blade.php

--- resources/views/layouts/page.blade.php
+++ resources/views/layouts/page.blade.php
@@ -11,8 +11,7 @@
         @show
     </head>
     <body>
-        @include('partials.menu',[
-            ])
+        @include('partials.menu',[])
         <div class="content my-5">
             @yield('content')
         </div>
