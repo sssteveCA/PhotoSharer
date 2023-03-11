e7ed4b8838e8441e859a00434e8cbc31046acbd8
stepp <stepp@mail.com>
2023-03-11 12:33:17 +0100

    Added response to PhotoAdminController.php update method

--- app/Http/Controllers/admin/PhotoAdminController.php
+++ app/Http/Controllers/admin/PhotoAdminController.php
@@ -8,6 +8,8 @@ use App\Models\Photo;
 use Illuminate\Http\Request;
 use App\Interfaces\Constants as C;
 use Exception;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class PhotoAdminController extends Controller
 {
@@ -40,7 +42,32 @@ class PhotoAdminController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try{
+            $request->validate([
+                'approved' => ['required',Rule::in([0,1])]
+            ]);
+            $approved = $request->input('approved');
+            $photo = Photo::find($id);
+            $photo->approved = $approved; 
+            $photo->save();
+            $status = $approved == 1 ? "approvato" : "non approvato";
+            $message = "Lo stato della foto è stato modificato in {$status}";
+            return response()->json([
+                C::KEY_DONE => true, C::KEY_MESSAGE => $message
+            ],200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(ValidationException){
+            return response()->json([
+                C::KEY_DONE => true, C::KEY_MESSAGE => 'Inserisci i dati richiesti per modificare la foto'
+            ],400,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(ResourceNotFoundException){
+            return response()->json([
+                C::KEY_DONE => false, C::KEY_MESSAGE => 'La foto richiesta non esiste'
+            ],404,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(Exception $e){
+            return response()->json([
+                C::KEY_DONE => false, C::KEY_MESSAGE => 'Errore durante la modifica della foto'
+            ],500,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }
     }
 
     /**
