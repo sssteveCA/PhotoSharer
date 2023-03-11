c0a93827c73bdee6ad79dfafe495598ef55797d7
stepp <stepp@mail.com>
2023-03-11 08:19:41 +0100

    Updated verify.blade.php and views in resources/views/auth/password folder

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -1,24 +1,24 @@
-@extends('layouts.app')
+@extends('layouts.page')
 
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verifica il tuo indirizzo email') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Abbiamo inviato un link di verifica al tuo indirizzo email.') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }},
+                    {{ __('Prima di procedere fai click sul link di verifica che abbiamo inviato alla tua casella di posta.') }}
+                    {{ __('Se non l\'hai ricevuto') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('fai click qui per richiederne un altro') }}</button>.
                     </form>
                 </div>
             </div>
