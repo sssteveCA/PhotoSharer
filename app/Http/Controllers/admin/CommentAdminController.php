b53dea3cad11068f299f65e3cee9fa8c1911c53f
stepp <stepp@mail.com>
2023-03-11 12:15:21 +0100

    Added response to CommentAdminController.php update method

--- app/Http/Controllers/admin/CommentAdminController.php
+++ app/Http/Controllers/admin/CommentAdminController.php
@@ -8,6 +8,8 @@ use Exception;
 use Illuminate\Http\Request;
 use App\Interfaces\Constants as C;
 use App\Models\Comment;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\ValidationException;
 
 class CommentAdminController extends Controller
 {
@@ -40,7 +42,32 @@ class CommentAdminController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        try{
+            $request->validate([
+                'approved' => ['required',Rule::in([0,1])]
+            ]);
+            $approved = $request->input('approved');
+            $comment = Comment::find($id);
+            $comment->approved = $approved; 
+            $comment->save();
+            $status = $approved == 1 ? "approvato" : "non approvato";
+            $message = "Lo stato del commento è stato modificato in {$status}";
+            return response()->json([
+                C::KEY_DONE => true, C::KEY_MESSAGE => $message
+            ],200,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(ValidationException){
+            return response()->json([
+                C::KEY_DONE => true, C::KEY_MESSAGE => 'Inserisci i dati richiesti per modificare il commento'
+            ],400,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(ResourceNotFoundException){
+            return response()->json([
+                C::KEY_DONE => true, C::KEY_MESSAGE => 'Il commento richiesto non esiste'
+            ],404,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }catch(Exception $e){
+            return response()->json([
+                C::KEY_DONE => false, C::KEY_MESSAGE => 'Errore durante la modifica del commento'
+            ],500,[],JSON_UNESCAPED_SLASHES|JSON_UNESCAPED_UNICODE);
+        }
     }
 
     /**
